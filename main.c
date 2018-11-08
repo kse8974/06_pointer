327594cebc96cfad2ed0a49f88b62ee6fe616f44
kse8974 <kse8974@naver.com>
2018-11-08 15:36:28 +0900

    06_practice1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i = 365;
+	int *ptr = &i;
+	
+	
+	printf("i address : %d \n", &i);
+	printf("i value : %d \n", i);
+	
+	printf("ptr address : %d \n", & *ptr);
+	printf("ptr value : %d \n", *ptr);
+	
+	
+	return 0;
+}
